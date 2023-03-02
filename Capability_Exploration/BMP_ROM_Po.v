fe269e02451ce68fe369d9cb9df5120a93f2aa4e
Ayandeep Hazra <ayandeephazra@hotmail.com>
2023-03-02 12:47:34 -0600

    bmp ok

--- Capability_Exploration/BMP_ROM_Po.v
+++ Capability_Exploration/BMP_ROM_Po.v
@@ -1,13 +1,13 @@
-module BMP_ROM_Pounce(clk,addr,dout);
+module BMP_ROM_Po(clk,addr,dout);
 
   input clk;     // 50MHz clock
   input [15:0] addr;
   output reg [8:0] dout;   // 9-bit color pixel out
 
-  reg [8:0] rom[0:5761];
+  reg [8:0] rom[0:6401];
 
   initial
-    $readmemh("Pounce.hex",rom);
+    $readmemh("Po.hex",rom);
 
   always @(posedge clk)
     dout <= rom[addr];
