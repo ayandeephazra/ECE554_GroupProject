5565a635e56f340e9f70f44fd0d4a9db4c77f878
Ayandeep Hazra <ayandeephazra@hotmail.com>
2023-03-21 17:00:15 -0500

    4 bit imm sig extended, no handling for zero extend (ask hoffman)

--- Capability_Exploration/id.v
+++ Capability_Exploration/id.v
@@ -336,7 +336,7 @@ always @(instr_IM_ID) begin
 	// rf_re1 = 1
 	ADDIi: begin
 	  rf_re0 = 1;					// read from reg 
-	  src1sel = IMM2SRC1_4BZE;		// access 4-bit ZE immediate
+	  src1sel = IMM2SRC1_4BSE;		// access 4-bit ZE immediate
 	  rf_we = 1;					// write as normal to a reg
 	  alu_func = ADD;				// use the "add" alu functionality but change the src muxes to get from immediates rather than reg file
       clk_z = 1;                    // include zero flags
@@ -344,7 +344,7 @@ always @(instr_IM_ID) begin
 	end
 	SUBIi: begin
 	 rf_re0 = 1;
-	 src1sel = IMM2SRC1_4BZE;
+	 src1sel = IMM2SRC1_4BSE;
 	 rf_we = 1;
 	 clk_z = 1;
 	 clk_nv = 1;
@@ -353,28 +353,28 @@ always @(instr_IM_ID) begin
 	end
 	XORIi: begin
 	  rf_re0 = 1;
-  	  rf_re1 = 1;
+  	  src1sel = IMM2SRC1_4BSE;
 	  rf_we = 1;
 	  clk_z = 1;	  
 	  alu_func = XOR;
 	end
 	ANDNIi: begin
 	  rf_re0 = 1;
-	  rf_re1 = 1;
+	  src1sel = IMM2SRC1_4BSE;
 	  rf_we = 1;
 	  clk_z = 1;
 	  alu_func = ANDN;
 	end
 	ANDIi: begin
 	 rf_re0 = 1;
-	 rf_re1 = 1;
+	 src1sel = IMM2SRC1_4BSE;
 	 rf_we = 1;
 	 clk_z = 1;
 	  alu_func = AND;
 	end
 	XORNIi: begin
 	  rf_re0 = 1;
-	  rf_re1 = 1;
+	  src1sel = IMM2SRC1_4BSE;
 	  rf_we = 1;
 	  clk_z = 1;
 	  alu_func = XOR;
@@ -382,7 +382,7 @@ always @(instr_IM_ID) begin
 	end
 	ORIi: begin
 	  rf_re0 = 1;
-	  rf_re1 = 1;
+	  src1sel = IMM2SRC1_4BSE;
 	  rf_we = 1;
 	  clk_z = 1;
 	  alu_func = OR;
--- Capability_Exploration/src_mux.v
+++ Capability_Exploration/src_mux.v
@@ -65,7 +65,7 @@ assign src0 = (src0sel_ID_EX == RF2SRC0) ? RF_p0 :
 
 assign src1 = (src1sel_ID_EX == RF2SRC1) ? RF_p1 : 
               (src1sel_ID_EX == NPC2SRC1) ? pc_ID_EX :	// for JAL
-			  (src1sel_ID_EX == IMM2SRC1_4BZE) ? {{12'h000},imm_ID_EX[3:0]} : // 4-bit zero extended immediate
+			  (src1sel_ID_EX == IMM2SRC1_4BSE) ? {{12{imm_ID_EX[3]}},imm_ID_EX[3:0]} : // 4-bit S-extended immediate, this is an append from org Hoffman implem
 			  {{8{imm_ID_EX[7]}},imm_ID_EX[7:0]};			// for LHB/LLB (sign extended 8-bit immediate
 			  
 endmodule
