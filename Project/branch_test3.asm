95096952abc0acc8e0f2cdd230fd4e101824a858
anish <anish@ad.engr.wisc.edu>
2023-03-21 18:27:26 -0500

    btb strong bit done

--- Project/branch_test3.asm
+++ Project/branch_test3.asm
@@ -19,8 +19,8 @@ llb R4, 0x6
 llb R4, 0x7
 sub R5, R2, R1
 sub R5, R5, R1
-b gt, SECOND        ## 1st. branch taken & not predicted -- PC: 11 -> PC: 16
-                    ## 2nd. branch not taken & predicted
+b gt, SECOND        ## 1st. branch taken & not predicted -- PC: 13 -> PC: 18
+                    ## 2nd. branch not taken & predicted (verify line is invalidated)
 
 llb R4, 0x8
 llb R4, 0x9
@@ -36,8 +36,8 @@ llb R4, 0xf
 sub R2, R2, R1      
 sub R5, R2, R1
 b eq, END           ## 1st. branch first not taken - not written
-                    ## 2nd. branch taken & not predicted
-b gte, REPEAT       ## REPEAT ONCE ----- PC: 23 -> PC: 2
+                    ## 2nd. branch taken & not predicted ---- PC: 24 -> PC: 26
+b gte, REPEAT       ## REPEAT ONCE ----- PC: 25 -> PC: 2
 
 END:
 b uncond, END
--- Project/btb.sv
+++ Project/btb.sv
@@ -20,9 +20,11 @@ logic [8:0] index;
 logic [6:0] tag;
 logic [24:0] btb_out;
 logic valid_bit, strong_bit;
+logic sbit_IF_ID, sbit_ID_EX;
+logic [15:0] target_PC_IF_ID, target_PC_ID_EX;
 logic en, btb_hit_IF_ID;
 
-logic write, alloc, evict;
+logic write, alloc, evict, set_strong, clr_strong;
 logic [8:0] wr_index;
 logic [25:0] btb_wr_data;
 
@@ -33,6 +35,8 @@ always_ff @(negedge clk) begin
   if (write)
     btb_mem[wr_index] <= btb_wr_data;
 end
+
+assign strong_bit = btb_out[17];
 
 // Determine hit
 assign valid_bit = btb_out[16];
@@ -46,27 +50,39 @@ assign target_PC = btb_out[13:0];
 // BTB Write logic
 /////////////////////
 always_comb begin
-  alloc = (~btb_hit_ID_EX & br_instr_ID_EX & flow_change_ID_EX);
-  evict = (btb_hit_ID_EX & flow_change_ID_EX);   // misprediction
-  write = alloc | evict;
+  alloc = (~btb_hit_ID_EX & br_instr_ID_EX & flow_change_ID_EX);  // taken and not predicted
+  evict = (btb_hit_ID_EX & flow_change_ID_EX & ~sbit_ID_EX);      // misprediction & no strong bit
+  set_strong = (btb_hit_ID_EX & ~flow_change_ID_EX);              // correct prediction
+  clr_strong = (btb_hit_ID_EX & flow_change_ID_EX & sbit_ID_EX);  // misprediiction & strong bit
+  write = alloc | evict | set_strong | clr_strong;
 
   wr_index = pc_ID_EX[8:0] - 1;   // pre-incremented PC
-  btb_wr_data = (alloc) ? {pc_ID_EX[15:9], 1'b0, 1'b1, dst_ID_EX} : {8'h0, 1'b0, 16'h0};
+  btb_wr_data = (alloc) ? {pc_ID_EX[15:9], 1'b0, 1'b1, dst_ID_EX} :
+                (set_strong) ? {pc_ID_EX[15:9], 1'b1, 1'b1, dst_ID_EX} :
+                (clr_strong) ? {pc_ID_EX[15:9], 1'b0, 1'b1, target_PC_ID_EX} :    // retain old prediction
+                {8'h0, 1'b0, 16'h0};    // invalidate line
 end
 
 	  
 /////////////////////////
 // Pipeline reg IF_ID //
 ///////////////////////
-always @(posedge clk)
+always @(posedge clk) begin
   btb_hit_IF_ID <= hit;
+  sbit_IF_ID <= strong_bit;
+  target_PC_IF_ID <= target_PC;
+end
+  
 	
 /////////////////////////
 // Pipeline reg ID_EX //
 ///////////////////////
-always @(posedge clk)
+always @(posedge clk) begin
   btb_hit_ID_EX <= btb_hit_IF_ID;   // Pipeline to EX to decide if flow change is necessary
-
+  sbit_ID_EX <= sbit_IF_ID;
+  target_PC_ID_EX <= target_PC_IF_ID;
+end
+  
 
 
 // use initial readmemh to reset all valid bits to 0. This will ysnthesize on the FPGA.
