b831d7e84e38bcab68ee390074d7576404d500e6
Ayandeep Hazra <ayandeephazra@hotmail.com>
2023-03-30 18:43:59 -0500

    MOVC final

--- Capability_Exploration/movc.asm
+++ Capability_Exploration/movc.asm
@@ -1,18 +1,18 @@
 llb R1, 0x55
 lhb R1, 0x55
-llb R2, 0x10
-lhb R2, 0x00
-llb R3, 0x0e
-lhb R3, 0xa9
+llb R2, 0x00
+lhb R2, 0x01
+llb R3, 0x49
+lhb R3, 0x00
 
-MOVC R4, R2, 0x01
+MOVC R4, R2, 0x00
 SUB R5, R4, R3
 B eq, PASS
 B neq, FAIL
 
 PASS:
 llb R6, 0x55
-lhb R6, 0xba
+lhb R6, 0x9a
 b uncond, END
 
 FAIL:
@@ -23,3 +23,7 @@ b uncond, END
 END:
 llb R9, 0x4d
 lhb R9, 0x0e
+b uncond, END
+
+MEM 0x0100
+STRING I am Here!
