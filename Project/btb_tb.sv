b33871f7b2404e43f4b21aa3e1a7b64061410bcc
anish <anish@ad.engr.wisc.edu>
2023-04-06 18:44:17 -0500

    Thurs: printing Hell

--- Project/btb_tb.sv
+++ Project/btb_tb.sv
@@ -2,19 +2,22 @@
 module btb_tb();
 
 logic clk, rst_n;
+wire [35:0] GPIO;
+logic TX, RX;
 
 // cpu iCPU(.clk(clk), .rst_n(rst_n), .wdata(), .mm_we(), .addr(), .mm_re(), .rdata());
-ce1 iCE1(.KEY({3'b000, rst_n}), .CLOCK_50(clk));
+BranchPrediction iTOPLEVEL(.KEY({3'b000, rst_n}), .CLOCK_50(clk), .GPIO(GPIO));
+
+assign TX = GPIO[3];
 
 initial begin
     clk = 0;
     rst_n = 0;
     @ (negedge clk)
     rst_n = 1;
-    iCE1.cpu1.iBTB.en = 1;   // enable btb
+    iTOPLEVEL.cpu1.iBTB.en = 1;   // enable btb
 
-
-    repeat (100) @ (posedge clk);
+    repeat (100000000) @ (posedge clk);
     $stop;
 
 end
