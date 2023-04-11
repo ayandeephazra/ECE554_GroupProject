059a49bb934f4e704caaeea2c949e980481f2ccc
Ayandeep Hazra <ayandeephazra@hotmail.com>
2023-04-11 18:49:45 -0500

    R9, R10 reserved

--- Capability_Exploration/game_code.asm
+++ Capability_Exploration/game_code.asm
@@ -14,14 +14,18 @@ LHB R4, 0xC0
 LLB R5, 0x04	# R5 contains TX/RX buffer (0xC004)
 LHB R5, 0xC0
 
+LLB R9, 0x00    # R9 stores the x-coordinates of the meteor
+
+LLB R10, 0x00	# R10 stores the y-coordinates of the meteor
+
 # Implement movement of spaceships
 # determine when to send meteor
 # determine when a collision between spaceship and meteors happen
 # display statistics of BTB
 
-# currently occupied registers: R0, R2, R3, R4, R5, R6, R7, R13, R14, R15
-# temp occupied registers: R6, R7, R8
-# free registers: R8, R9, R10, R11, R12
+# currently occupied registers: R0, R2, R3, R4, R5, R6, R7, R13, R14, R15, R9, R10
+# temp occupied registers: R6, R7, R8,
+# free registers: R11, R12
 
 GAME_LOOP:
 
@@ -32,7 +36,7 @@ LW R6, R2, 0		# read data (key_press[1]) located in C000 into R6
 LW R7, R3, 0		# read data (key_press[2]) located in C001 into R7
 
 SUBI R8, R6, 0x01 	# check if key[1] (up) is pressed
-B eq, SPACESHIP_MOVEMENT
+B eq, SPACE_SHIP_MOVEMENT
 
 SUBI R8, R7, 0x01	# check if key[2] (down) is pressed
 B eq, SPACE_SHIP_MOVEMENT 
@@ -40,6 +44,10 @@ B eq, SPACE_SHIP_MOVEMENT
 # Game stats updated and sent to display here:
 
 # Game end condition here:
+COLLISION_CHECK:	# check if collision after a move
+
+COLLISION_HANDLING:	# if collision, handle here
+
 B eq, END	# if the spaceship position and meteor position are touching, end game
 
 # Generate random number from LFSR for picture display here:
