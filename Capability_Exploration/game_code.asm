e18a7668843cc6318f6d1a283c56d5a046c361a8
Ayandeep Hazra <ayandeephazra@hotmail.com>
2023-04-13 15:26:21 -0500

    Update game_code.asm

--- Capability_Exploration/game_code.asm
+++ Capability_Exploration/game_code.asm
@@ -5,27 +5,36 @@
 LLB R2, 0x00	# R2 contains addr for key[1] (0xC000)... (move up)
 LHB R2, 0xC0	
 
-LLB R3, 0x01	# R3 contains addr for key[2] (0xC001)... (move down)
-LHB R3, 0xC0	
+# can all be done by immediate addition to R2
+	#LLB R3, 0x01	# R3 contains addr for key[2] (0xC001)... (move down)
+	#LHB R3, 0xC0	
 
-LLB R4, 0x05	# R4 contains status register (0xC005)
-LHB R4, 0xC0
+	#LLB R4, 0x05	# R4 contains status register (0xC005)
+	#LHB R4, 0xC0
 
-LLB R5, 0x04	# R5 contains TX/RX buffer (0xC004)
-LHB R5, 0xC0
+	#LLB R5, 0x04	# R5 contains TX/RX buffer (0xC004)
+	#LHB R5, 0xC0
 
-LLB R9, 0x00    # R9 stores the x-coordinates of the lower left corner of the meteor
+LLB R9, 0x00    # R9 stores the x-coordinates of the lower left corner of the 
+				# meteor at any one instance of the gameloop
 
-LLB R10, 0x00	# R10 stores the y-coordinates of the lower left corner of the meteor
+LLB R10, 0x00	# R10 stores the y-coordinates of the lower left corner of the 
+				# meteor at any one instance of the gameloop
+				
+LLB R11, 0x00	# R11 stores the x-coordinates of the lower left corner of the 
+				# spaceship at any one instance of the gameloop
+				
+LLB R12, 0x00	# R11 stores the y-coordinates of the lower left corner of the 
+				# spaceship at any one instance of the gameloop
 
 # Implement movement of spaceships
 # determine when to send meteor
 # determine when a collision between spaceship and meteors happen
 # display statistics of BTB
 
-# currently occupied registers: R0, R2, R3, R4, R5, R6, R7, R13, R14, R15, R9, R10
+# currently occupied registers: R0, R2, R3, R4, R5, R6, R7, R13, R14, R15, R9, R10, R11, R12
 # temp occupied registers: R6, R7, R8,
-# free registers: R11, R12
+# free registers: 
 
 GAME_LOOP:
 
@@ -33,7 +42,7 @@ GAME_LOOP:
 
 # picture movement begins here:
 LW R6, R2, 0		# read data (key_press[1]) located in C000 into R6
-LW R7, R3, 0		# read data (key_press[2]) located in C001 into R7
+LW R7, R2, 1		# read data (key_press[2]) located in C001 into R7
 
 SUBI R8, R6, 0x01 	# check if key[1] (up) is pressed
 B eq, SPACE_SHIP_MOVEMENT
@@ -45,18 +54,26 @@ B eq, SPACE_SHIP_MOVEMENT
 
 # Game end condition here:
 COLLISION_CHECK:	# check if collision after a move
-LLB R11, 0x50       # dimensions of spaceship 5*16+0=80
-PUSH R11
-PUSH R11
 # branch to handling iff 1, 2 amd 3 hold simultaneously 
-# 1. x coordinate of lower left of spaceship + decimal 100 >= lower left x coordinate of meteor (R9)
-# 2. y coordinate of lower left of spaceship <= lower left y coordinate of meteor (R10)
-# 3. y coordinate of lower left of spaceship + decimal 100 >= lower left y coordinate of meteor (R10) + decimal 80
+# 1. x coordinate of lower left of spaceship (R11) + decimal 100 >= lower left x coordinate of meteor (R9)
+# 2. y coordinate of lower left of spaceship (R12) <= lower left y coordinate of meteor (R10)
+# 3. y coordinate of lower left of spaceship (R12) + decimal 100 >= lower left y coordinate of meteor (R10) + decimal 60
 #   \---- 
 #    \-------\            /\
 #    /-------/            \/
 #   /----
-
+LLB R1, 0x64       # dimensions of spaceship 6*16+4=100
+ADD R1, R11, R1
+SUB R1, R9, R1
+B lte, COLLISION_HANDLING
+SUB R1, R12, R10
+B lte, COLLISION_HANDLING
+LLB R1, 0x64       # dimensions of spaceship 6*16+4=100
+ADD R6, R11, R1
+LLB R1, 0x3C       # dimensions of meteor 3*16+12=60
+ADD R7, R10, R1
+SUB R1, R7, R6
+B lte, COLLISION_HANDLING
 
 COLLISION_HANDLING:	# if collision, handle here
 
