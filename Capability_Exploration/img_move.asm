ef26873bdde0cd7f05c38f0ef476c33ebd306ffa
Joshua Bowers <josh@leocancercare.com>
2023-04-13 18:55:53 -0500

    button presses don't work, broken movement

--- Capability_Exploration/img_move.asm
+++ Capability_Exploration/img_move.asm
@@ -20,9 +20,13 @@ sw R3, R1, 1                # YLOC <= 8'hF0 (center of the screen)
 llb R4, 0x3                 # cntrl <= 3
 sw R4, R1, 2
 
+llb R12, 0x00     
+lhb R12, 0x40
 JAL COUNT                   # Gonna have to figure out how we do delay bc of the branch prediction
 
-B uncond, END
+b uncond, RUN
+
+b uncond, END
 
 ################################################
 # REGS IN USE #
@@ -31,60 +35,65 @@ B uncond, END
 # R3 - YLOC (temp)
 # currently there's no register that saves xloc & yloc, so need to be mmaped
 
-# RUN:
-#     # If KEY[2] pressed:   
-#     LW R12, R10, 0		    # load up register
-#     ANDI R12, R12, 1		# Is KEY[2] being pressed?
-#     B EQ, UP	            # if yes, go to UP
+RUN:
+    # If KEY[2] pressed:   
+    LLB R13, 0x01		   
+    LW R12, R10, 0		    # load up register
+    AND R12, R12, R13		# Is KEY[2] being pressed?
+    B EQ, UP	            # if yes, go to UP
     
-#     # If KEY[1] pressed:
-#     LW R12, R11, 0		    # load down register
-#     AND R11, R11, R12		# Is KEY[1] being pressed?
-#     B EQ, DOWN              # if yes, go to DOWN
+    # If KEY[1] pressed:
+    LLB R13, 0x02		   
+    LW R12, R11, 0		    # load down register
+    AND R12, R12, R13		# Is KEY[1] being pressed?
+    B EQ, DOWN              # if yes, go to DOWN
 
-#     # loop back
-#     B uncond, RUN
+    # loop back
+    B uncond, RUN
 
+UP:
+    # B uncond, EDGETOP       # check top edge
 
-# UP:
-#     B uncond, EDGETOP       # check top edge
+    ADDI R3, R3, 1          # move img down by 1 units
+    sw R2, R1, 0            # XLOC <= 10'h0000 (left side of the screen)
+    sw R3, R1, 1
+    llb R4, 0x3             # cntrl <= 3
+    sw R4, R1, 2            # print new location
 
-#     ADDI R3, R3, 1          # move img down by 1 units
-#     sw R3, R1, 0
-#     llb R4, 0x3             # cntrl <= 3
-#     sw R4, R1, 2            # print new location
-
-#     JAL COUNT               #### WAIT 1000 cycles 
-#     B uncond, RUN:
-
-# DOWN:
-#     B uncond, EDGEBOTTOM    # check bottom edge
-
-#     SUBI R3, R3, 1          # move img down by 1 units
-#     sw R3, R1, 0
-#     llb R4, 0x3             # cntrl <= 3
-#     sw R4, R1, 2            # print new location
-
-#     JAL COUNT               #### WAIT 1000 cycles 
-#     B uncond, RUN
-
-# EDGEBOTTOM:
-#     SUBI R4, R3, 0          # if R3 <= 0, then at bottom edge 
-#     B LTE, RUN              # can't move anymore, go to RUN
-
-#     B uncond, DOWN
-
-# EDGETOP:
-#     llb R4, 0xE0            # R4 holds top bound (480)
-#     lhb R4, 0x01
-#     SUB R4, R4, R3          # if R4 >= 0, then at top edge
-#     B LTE, RUN              # can't move anymore, go to RUN
-
-#     B uncond, UP
-
-COUNT:
     llb R12, 0x00     
     lhb R12, 0x40
+    JAL COUNT               #### WAIT 1000 cycles 
+    B uncond, RUN
+
+DOWN:
+    # B uncond, EDGEBOTTOM    # check bottom edge
+
+    SUBI R3, R3, 1          # move img down by 1 units
+    sw R2, R1, 0            # XLOC <= 10'h0000 (left side of the screen)
+    sw R3, R1, 1
+    llb R4, 0x3             # cntrl <= 3
+    sw R4, R1, 2            # print new location
+
+    llb R12, 0x00     
+    lhb R12, 0x40
+    JAL COUNT               #### WAIT 1000 cycles 
+    B uncond, RUN
+
+#EDGEBOTTOM:
+#    SUBI R4, R3, 0          # if R3 <= 0, then at bottom edge 
+#    B LTE, RUN              # can't move anymore, go to RUN
+
+#    B uncond, DOWN
+
+#EDGETOP:
+#    llb R4, 0xE0            # R4 holds top bound (480)
+#    lhb R4, 0x01
+#    SUB R4, R4, R3          # if R4 >= 0, then at top edge
+#    B LTE, RUN              # can't move anymore, go to RUN
+
+#    B uncond, UP
+
+COUNT:
     SUBI R12, R12, 1
     B NEQ, COUNT
     JR R15
