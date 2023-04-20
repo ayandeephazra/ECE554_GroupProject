3315983b07b8e03f6c79d0829efa9eb468bb3f3d
Josh <83430896+JoshhWB@users.noreply.github.com>
2023-04-20 17:37:56 -0500

    final movement

--- Project/img_move.asm
+++ Project/img_move.asm
@@ -28,29 +28,27 @@ b uncond, END
 # REGS IN USE #
 # R1 - bmp mmap address
 # R3 - YLOC (temp)
-# R10 - KEY[1] mmap address
-# R11 - KEY[2] mmap address
-# currently there's no register that saves xloc & yloc, so need to be mmaped
+# R10 - mmap address for mmap_regs
 
 RUN:
     # If KEY[1] pressed:   
-    llb R13, 0xFF	        # should be 16'hFFFF   
-    lhb R13, 0xFF            
+    llb R13, 0xF0	        # should be 16'hF0F0   
+    lhb R13, 0xF0            
 
     lw R12, R10, 4		    # load up register -> 0xc014
-    and R12, R12, R13		# Is KEY[2] being pressed?
+    sub R12, R12, R13		# Is KEY[2] being pressed?
     b eq, UP	            # if eq, go to UP
     
     # If KEY[2] pressed:   
     lw R12, R10, 5		    # load down register -> 0xc015
-    and R12, R12, R13		# Is KEY[1] being pressed?
+    sub R12, R12, R13		# Is KEY[1] being pressed?
     b eq, DOWN              # if eq, go to DOWN
 
     # loop back otherwise
     b uncond, RUN
 
 UP:
-    # B uncond, EDGETOP       # check top edge
+    B uncond, EDGETOP       # check top edge
 
     addi R3, R3, 1          # move img down by 1 units
     sw R2, R1, 0            # XLOC <= 10'h0000 (left side of the screen)
@@ -64,7 +62,7 @@ UP:
     b uncond, RUN
 
 DOWN:
-    # B uncond, EDGEBOTTOM    # check bottom edge
+    B uncond, EDGEBOTTOM    # check bottom edge
 
     subi R3, R3, 1          # move img down by 1 units
     sw R2, R1, 0            # XLOC <= 10'h0000 (left side of the screen)
@@ -77,19 +75,21 @@ DOWN:
     JAL COUNT               #### WAIT 1000 cycles 
     b uncond, RUN
 
-#EDGEBOTTOM:
-#    SUBI R4, R3, 0          # if R3 <= 0, then at bottom edge 
-#    B LTE, RUN              # can't move anymore, go to RUN
+# check this code again
+EDGEBOTTOM:
+    SUBI R4, R3, 0          # if R3 <= 0, then at bottom edge 
+    B LTE, RUN              # can't move anymore, go to RUN
 
-#    B uncond, DOWN
+    B uncond, DOWN
 
-#EDGETOP:
-#    llb R4, 0xE0            # R4 holds top bound (480)
-#    lhb R4, 0x01
-#    SUB R4, R4, R3          # if R4 >= 0, then at top edge
-#    B LTE, RUN              # can't move anymore, go to RUN
+# check this code again
+EDGETOP:
+    llb R4, 0xE0            # R4 holds top bound (480)
+    lhb R4, 0x01
+    SUB R4, R4, R3          # if R4 >= 0, then at top edge
+    B LTE, RUN              # can't move anymore, go to RUN
 
-#    B uncond, UP
+    B uncond, UP
 
 COUNT:
     SUBI R12, R12, 1
