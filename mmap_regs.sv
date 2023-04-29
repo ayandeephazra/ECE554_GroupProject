93419dc94b1ca348e2c62a5e0a38ba32610f6f0f
brodyskaleski <104120265+brodyskaleski@users.noreply.github.com>
2023-04-28 19:58:10 -0500

    Add files via upload

--- mmap_regs.sv
+++ mmap_regs.sv
@@ -1,10 +1,11 @@
 module mmap_regs(
     input clk, 
     input rst_n,
-    input br_stats_wr,
-    input mmap_re,          // read to mmap regs
-    input [3:0] mmap_addr,
-    inout [15:0] databus,   // input (for en/dis stats count) OR output (mmap_reg data)
+    input br_stats_wr,      // enable banch prediction stats recording
+    input lfsr_load,        // write SEED to lfsr
+    input mm_re,            // external read
+    input [15:0] addr,
+    inout [15:0] databus,   // input (for en/dis stats count || lfsr seed) OR output (mmap_reg data)
     input inc_br_cnt,
     input inc_hit_cnt,
     input inc_mispr_cnt,
@@ -16,22 +17,25 @@ module mmap_regs(
     logic [15:0] timer;
     logic stats_en;     // a write to mem addr 0xc00b sets or clears the stats reg
     logic [15:0] button_up, button_down;
+    logic [7:0] lfsr_out, lfsr_SEED;
 
     // mmap_addr needs to check all 4 bits -> switched to hex to make it easier
-    assign databus = (mmap_re & mmap_addr == 4'h0) ? br_cnt :
-                     (mmap_re & mmap_addr == 4'h1) ? mispr_cnt :
-                     (mmap_re & mmap_addr == 4'h2) ? hit_cnt :
-                     (mmap_re & mmap_addr == 4'h3) ? timer :
-                     (mmap_re & mmap_addr == 4'h4) ? button_up :       
-                     (mmap_re & mmap_addr == 4'h5) ? button_down :       
+    assign databus = (mm_re & addr == 16'hc010) ? br_cnt :
+                     (mm_re & addr == 16'hc011) ? mispr_cnt :
+                     (mm_re & addr == 16'hc012) ? hit_cnt :
+                     (mm_re & addr == 16'hc013) ? timer :
+                     (mm_re & addr == 16'hc014) ? button_up :       
+                     (mm_re & addr == 16'hc015) ? button_down :
+                     (mm_re & addr == 16'hc016) ? lfsr_out :
                      16'hzzzz;
 
+    // enable branch prediction stats count -- addr 0xc00b
     always_ff @ (posedge clk, negedge rst_n)
         if (!rst_n)
             stats_en <= '0;
         else if (br_stats_wr)
             stats_en <= databus[0];
-    // LFSR
+
     // Branch count -- addr 0xc010
     always_ff @(posedge clk, negedge rst_n)
         if (!rst_n)
@@ -65,7 +69,7 @@ module mmap_regs(
         if (!rst_n)
             button_up <= 16'h0000;
         else if (KEY_UP)
-            button_up <= 16'hFFFF;
+            button_up <= 16'hF0F0;
         else
             button_up <= 16'h0000;
 
@@ -74,9 +78,17 @@ module mmap_regs(
         if (!rst_n)
             button_down <= 16'h0000;
         else if (KEY_DOWN)
-            button_down <= 16'hFFFF;
+            button_down <= 16'hF0F0;
         else
             button_down <= 16'h0000;
+
+    assign lfsr_SEED = (lfsr_load) ? databus[7:0] : 8'h0;
+
+    ////////////////////////
+	// Instantiate LFSR  //
+	//////////////////////
+	LFSR iLFSR(.clk(clk), .rst_n(rst_n), .load(lfsr_load), .SEED(lfsr_SEED), .q(lfsr_out));
+
     
 
 endmodule
