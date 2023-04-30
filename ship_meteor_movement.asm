ff58299e3c8d2e1515b3ceea87f50a93fd3eb267
brodyskaleski <104120265+brodyskaleski@users.noreply.github.com>
2023-04-30 16:41:33 -0500

    up to date

--- ship_meteor_movement.asm
+++ ship_meteor_movement.asm
@@ -11,35 +11,8 @@ lhb R3, 0x00
 llb R10, 0x10               # R10 holds mmap address for mmap_regs
 lhb R10, 0xC0
 
-# R2 used temporarily
-# x location of image deletion
-llb R2, 0x10  # 0x10
-lhb R2, 0x00
-sw R2, R1, 0
-
-# R2 used temporarily
-# C00B stores y location of last movement
-lw R2, R1, 3
-#llb R2, 0x72  # 0x10
-#lhb R2, 0x01
-sw R2, R1, 1
-
-llb R4, 0x05                 # cntrl <= 5 + 1 for removing existing image
-lhb R4, 0xFF
-sw R4, R1, 2
-
-llb R12, 0xFE     
-lhb R12, 0x79
-
-REM_SHIP_IF_EXISTS:
-SUBI R12, R12, 1
-B NEQ, REM_SHIP_IF_EXISTS
-	
-##############################################################################
-############################# Place Ship #####################################
-##############################################################################
-
-llb R2, 0x10                # XLOC <= 10'h0010 (left side of the screen)
+####### Place Ship
+llb R2, 0x10                # XLOC <= 10'h0080 (left side of the screen)
 lhb R2, 0x00
 sw R2, R1, 0                
 
@@ -54,10 +27,6 @@ lhb R12, 0x40
 PLACE_SHIP_COUNT:
 SUBI R12, R12, 1
 B NEQ, PLACE_SHIP_COUNT
-
-#push R15
-#JAL COUNT                   # Gonna have to figure out how we do delay bc of the branch prediction
-#pop R15
 
 GAME_LOOP:
 # place meteor here:
@@ -89,10 +58,7 @@ RUN:
     b eq, DOWN                      # if eq, go to DOWN
 
     # loop back otherwise
-#    JAL RIGHT_TO_LEFT
    b uncond, RIGHT_TO_LEFT
-
-#    b uncond, RUN
 
 DOWN:
     # check bottom of screen
@@ -102,76 +68,46 @@ DOWN:
     SUB R4, R4, R3          # if R4 == R3, then at bottom of screen
     b neq, MOVING_DOWN
 
-    #push R15
-    #JAL RIGHT_TO_LEFT
-    #pop R15
    b uncond, RIGHT_TO_LEFT
-    #b uncond, RUN
 
 MOVING_DOWN:
     addi R3, R3, 1          # move img down by 1 units
-	llb R2, 0x10            # XLOC <= 10'h0010 (left side of the screen)
-	lhb R2, 0x00
-    sw R2, R1, 0            # XLOC <= 10'h0010 (left side of the screen)
+    sw R2, R1, 0            # XLOC <= 10'h0080 (left side of the screen)
     sw R3, R1, 1
     llb R4, 0x3             # cntrl <= 3
     sw R4, R1, 2            # print new location
 
     llb R12, 0x00     
     lhb R12, 0x40
-	
-	sw R3, R1, 3			# STORING in memory the y coord of spaceship
 
     MOVING_DOWN_COUNT:
-	sw R3, R1, 3
     SUBI R12, R12, 1
     B NEQ, MOVING_DOWN_COUNT
-	
-	sw R3, R1, 3			# STORING in memory the y coord of spaceship
 
- #   push R15
-  #  JAL RIGHT_TO_LEFT
-   # pop R15
 b uncond, RIGHT_TO_LEFT
-   # b uncond, RUN
 
 UP:
     # check top of screen
     SUBI R4, R3, 0          # if R3 == 0, then at top of screen
     b neq, MOVING_UP
     b eq, RIGHT_TO_LEFT
-    #push R15
-    #JAL RIGHT_TO_LEFT
-    #pop R15
-    #b uncond, RUN
 
 MOVING_UP:
 
     subi R3, R3, 1          # move img up by 1 units
-	llb R2, 0x10            # XLOC <= 10'h0010 (left side of the screen)
-	lhb R2, 0x00
-    sw R2, R1, 0            # XLOC <= 10'h0010 (left side of the screen)
+    sw R2, R1, 0            # XLOC <= 10'h0080 (left side of the screen)
     sw R3, R1, 1
     llb R4, 0x3             # cntrl <= 3
     sw R4, R1, 2            # print new location
-	
+
     llb R12, 0x00     
     lhb R12, 0x40
-	
-	sw R3, R1, 3			# STORING in memory the y coord of spaceship
 
     MOVING_UP_COUNT:
-	sw R3, R1, 3
     SUBI R12, R12, 1
     B NEQ, MOVING_UP_COUNT
-	
-	sw R3, R1, 3			# STORING in memory the y coord of spaceship
-	
-    #push R15
-    #JAL RIGHT_TO_LEFT
-    #pop R15
+
    b uncond, RIGHT_TO_LEFT
-#    b uncond, RUN
 
 RIGHT_TO_LEFT:
 
@@ -191,26 +127,21 @@ RIGHT_TO_LEFT:
 	llb R12, 0x00
 	lhb R12, 0x40			# for COUNT
 
-	#llb R12, 0xFF
-	#lhb R12, 0x79
-
     RIGHT_TO_LEFT_COUNT:
     SUBI R12, R12, 1
     B NEQ, RIGHT_TO_LEFT_COUNT
 
 	sub R6, R6, R7			# if R3 = FC04
-	B neq, RUN              #REMOVE_IMAGE		# remove the image
-	#JR R15				# jump to whereever the check was done at in ship movement
+	B eq, REMOVE_IMAGE		# remove the image
+	b neq, RUN
 
-	#b uncond, RUN
+REMOVE_IMAGE:
 
-REMOVE_IMAGE:	
-	#sw R6, R1, 0	# blackout image
 	sw R7, R1, 0
-	sw R5, R1, 1	# write y to mem
+	sw R5, R1, 1	
 
-	llb R4, 0x05	# cntrl <= 7 for blackout
-	lhb R4, 0xFF
+	llb R4, 0x00	# cntrl <= 7 for blackout
+	lhb R4, 0x80
 	sw R4, R1, 2	# remove img
 
 	llb R12, 0x00	# counter value
@@ -220,11 +151,6 @@ REMOVE_IMAGE:
     SUBI R12, R12, 1
     B NEQ, REMOVE_IMAGE_COUNT
 
-#	push R15
-#	JAL COUNT
-#	pop R15
-
-#	JR R15		# jump to wherever the check was done at in ship movement
 	B uncond, PLACE_METEOR
 
 PLACE_METEOR: # meteor lanes will eventually go here
@@ -248,13 +174,7 @@ PLACE_METEOR_COUNT:
 SUBI R12, R12, 1
 B NEQ, PLACE_METEOR_COUNT
 
-#push R15	# push R15 into queue
-#JAL COUNT	# jump to count
-#pop R15		# remove R15 onto stack
-
 b uncond, RUN 	# cause of bug
-
-#JR R15	
 
 COUNT:
     SUBI R12, R12, 1
