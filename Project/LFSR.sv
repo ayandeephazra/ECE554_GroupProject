a517abc13ec463fa387c87d187eebd0c5097229b
Josh Bowers <jwbowers@wisc.edu>
2023-05-06 20:30:18 -0500

    added comments

--- Project/LFSR.sv
+++ Project/LFSR.sv
@@ -6,10 +6,12 @@ input               load;           // load the SEED into the LFSR
 input [7:0]         SEED;           // preset for the LSFR
 output logic [7:0]  q;              // output of the LFSR
 
+// flip-flops used in the LFSR
 logic ff8, ff7, ff6, ff5, ff4, ff3, ff2, ff1;
 
+
 always_ff @(posedge clk, negedge rst_n) begin
-    if (!rst_n) begin
+    if (!rst_n) begin               // set all ffs to 0 on rst_n low
         ff8 <= 0;
         ff7 <= 0;
         ff6 <= 0;
@@ -19,7 +21,7 @@ always_ff @(posedge clk, negedge rst_n) begin
         ff2 <= 0;
         ff1 <= 0;
     end
-    else if (load) begin
+    else if (load) begin            // load seed into each ff
         ff8 <= SEED[7];
         ff7 <= SEED[6];
         ff6 <= SEED[5];
@@ -29,18 +31,19 @@ always_ff @(posedge clk, negedge rst_n) begin
         ff2 <= SEED[1];
         ff1 <= SEED[0];
     end
-    else begin
+    else begin                      // shift each bit one ff forward
         ff8 <= ff1;
         ff7 <= ff8;
         ff6 <= ff7;
         ff5 <= ff6;
-        ff4 <= ff5 ^ ff1;
-        ff3 <= ff4 ^ ff1;
-        ff2 <= ff3 ^ ff1;
+        ff4 <= ff5 ^ ff1;           // XOR ff5 and ff1 to add "randomness"    
+        ff3 <= ff4 ^ ff1;           // XOR ff4 and ff1 to add "randomness"
+        ff2 <= ff3 ^ ff1;           // XOR ff3 and ff1 to add "randomness"
         ff1 <= ff2;
+        
+        // output
         q <= {ff8, ff7, ff6, ff5, ff4, ff3, ff2, ff1};
     end
 end
-
 
 endmodule
--- Project/LFSR_tb.sv
+++ Project/LFSR_tb.sv
@@ -1,19 +1,25 @@
 module LFSR_tb();
 
-reg clk;
-reg rst_n, load;
-reg [7:0] SEED, q;
+reg             clk;            // 50MHz clock 
+reg             rst_n, load;    // reset low, load signal for LFSR
+reg [7:0]       SEED, q;        // 8-bit value to go into LFSR, 8-bit output
 
+///////////////////////
+// Instantiate LFSR //
+/////////////////////
 LFSR iDUT(.clk(clk), .rst_n(rst_n), .load(load), .SEED(SEED), .q(q));
 
 initial begin
+    // start clk at 0 and rst low
     clk = 0;
     rst_n = 0;
     SEED = 8'h88;
     repeat(5)@(posedge clk);
+    // load seed and rst high
     rst_n = 1;
     load = 1;
     repeat(5)@(posedge clk);
+    // start LFSR
     load = 0;
     rst_n = 1;
     
@@ -21,9 +27,11 @@ initial begin
     rst_n = 0;
     SEED = 8'h12;
     repeat(5)@(posedge clk);
+    // load seed and rst high
     load = 1;
     rst_n = 1;
     repeat(5)@(posedge clk);
+    // start LFSR
     rst_n = 1;
     load = 0;
 
@@ -31,9 +39,11 @@ initial begin
     rst_n = 0;
     SEED = 8'hE4;
     repeat(5)@(posedge clk);
+    // load seed and rst high
     load = 1;
     rst_n = 1;
     repeat(5)@(posedge clk);
+    // start LFSR
     rst_n = 1;
     load = 0;
 
@@ -41,9 +51,11 @@ initial begin
     rst_n = 0;
     SEED = 8'hFF;
     repeat(5)@(posedge clk);
+    // load seed and rst high
     load = 1;
     rst_n = 1;
     repeat(5)@(posedge clk);
+    // start LFSR
     rst_n = 1;
     load = 0;
 
@@ -51,9 +63,11 @@ initial begin
     rst_n = 0;
     SEED = 8'h00;
     repeat(5)@(posedge clk);
+    // load seed and rst high
     load = 1;
     rst_n = 1;
     repeat(5)@(posedge clk);
+    // start LFSR
     rst_n = 1;
     load = 0;
 
